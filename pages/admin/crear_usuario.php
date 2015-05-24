da91bb8585c114ca21bd13c924ca3e99493a71ea
lalomtz <lalomtzmunoz@gmail.com>
2015-05-24 15:48:58 -0500

    Update crear_usuario.php

--- pages/admin/crear_usuario.php
+++ pages/admin/crear_usuario.php
@@ -11,6 +11,55 @@
         <?php include ("header.html"); ?>
         <?php include ("nav.php"); ?>
         <!-- Contenido -->
+<form method="post" action="<?php $_PHP_SELF ?>">
+<table width="400" border="0" cellspacing="1" cellpadding="2">
+<tr>
+<td width="100">Numero de control</td>
+<td><input name="usuarios_id_usuario" type="text" id="usuarios_id_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Password</td>
+<td><input name="usuarios_password_usuario" type="password" id="usuarios_password_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Nombres</td>
+<td><input name="usuarios_nombres_usuario" type="text" id="usuarios_nombres_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Apellidos</td>
+<td><input name="usuarios_apellidos_usuario" type="text" id="usuarios_apellidos_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Telefono</td>
+<td><input name="usuarios_telefono_usuario" type="text" id="usuarios_telefono_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Carrera</td>
+<td><input name="usuarios_carrera_usuario" type="text" id="usuarios_carrera_usuario"></td>
+</tr>
+
+<tr>
+<td width="100">Horario</td>
+<td><input name="usuarios_horario_usuario" type="text" id="usuarios_horario_usuario"></td>
+</tr>
+
+<tr>
+<td width="100"> </td>
+<td> </td>
+</tr>
+<tr>
+<td width="100"> </td>
+<td>
+<input name="add" type="submit" id="add" value="Añadir usuario">
+</td>
+</tr>
+</table>
+</form>
         
         
         
