3ee0870b693aecc8d7439381c6ed47fdbb107824
emiliohdez <emilio.e.hdez@hotmail.com>
2015-05-27 17:09:15 -0500

    Update cuenta.php
    No lo he probado a causa de lo de la Base de Datos :D!

--- pages/admin/cuenta.php
+++ pages/admin/cuenta.php
@@ -10,9 +10,46 @@
        <div class="wrapper">
         <?php include ("header.html"); ?>
         <?php include ("nav.php"); ?>
+        
         <!-- Contenido -->
         
         
+        <!-- No se si el cambio de contraseña va aquí e.e -->
+        <h1>Cambio de contraseña de un usuario o administrador</h1>
+        <form method="post" action="<?php $_PHP_SELF ?>">
+            <div class="elemento">
+                <label for="usuarios_id_usuario">Nickname o RFC</label>
+                <input name="usuarios_id_usuario" type="text" id="usuarios_id_usuario" placeholder="Nickname/RFC" title="no compatible"/>
+                <span class="rojo">*</span>
+            </div>
+            
+            <div class="elemento">
+                <label for="usuarios_clave_usuario">Nueva Contraseña</label>
+                <input name="usuarios_clave_usuario" type="password" id="usuarios_clave_usuario" placeholder="Nueva contraseña" title="no compatible"/>
+                <span class="rojo">*</span>
+            </div>
+            
+            <div class="elemento">
+                <input name="add" class="boton" type="submit" id="add" value="Añadir usuario">
+            </div>
+        </form>
+        
+        
+        <!-- Kind of PHP -->
+        <?php 
+            include ("../../db/conexion.php");
+            $query = "UPDATE usuarios SET usuarios.password_usuario = 'default' WHERE usuarios.id_usuario = '".$clave."'";
+            $result = $linkConexion->query($query) or die("Error in the consulta y así ._.!.." . mysqli_error($linkConexion));
+            if (!$result) {
+                echo mysql_error();
+                echo "No se pudo realizar";
+            } else {
+                echo "<script>alert('Listo')</script>";
+            }
+        ?>
+        <!-- Kind of PHP -->
+        
+        <!-- cambio de contraseña... EOF -->
         
         
         
